07c18e11bd7667e3c045c3d0f518da74bf27043c
mateusz <mat.sem@wp.pl>
2022-12-21 19:31:09 +0100

    init

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        EntityManagerFactory emf= Persistence.createEntityManagerFactory("default");
+        EntityManager em=emf.createEntityManager();
+        EntityTransaction t=em.getTransaction();
+        t.begin();
+        Users user=new Users();
+        user.setName("mateusz");
+        user.setLastName("semklo");
+        em.persist(user);
+        t.commit();
+        em.close();
     }
 }
--- src/main/java/org/example/Users.java
+++ src/main/java/org/example/Users.java
@@ -1,4 +1,40 @@
 package org.example;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
 public class Users {
+    @Id
+    @GeneratedValue(generator = "ID_GENERATOR")
+    @GenericGenerator(name="ID_GENERATOR", strategy = "increment")
+    @Column(name = "ID")
+    int id;
+
+    @Column(name = "NAME")
+    String name;
+
+    @Column(name="LASTNAME")
+    String lastName;
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
